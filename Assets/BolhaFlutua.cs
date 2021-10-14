1781ee37b245b6b9b743cb7167fd398c7485c457
Ana Beatryz Yoshie <anabeatryz_y@outlook.com>
2021-10-14 14:24:39 -0300

    Pulo e ajuste de colisores

--- Assets/BolhaFlutua.cs
+++ Assets/BolhaFlutua.cs
@@ -6,25 +6,32 @@ public class BolhaFlutua : MonoBehaviour
 {
     private Rigidbody2D _myRigidbody;
     [SerializeField] private Vector2 bolhaForce;
-    //atributo, modificacoes que pode-se empregar a uma classe ou variavel
+   // private bool morreu;
+    
 
     void Start()
     {
        _myRigidbody = GetComponent<Rigidbody2D>();
-        //generic, retorna qualquer coisa na classe component
+        
     }
 
     public void Flutua()
     {
+        //if (morreu)
+       // {
+           
+       // }
+
        _myRigidbody.velocity = Vector2.zero;
        _myRigidbody.AddForce(bolhaForce, ForceMode2D.Impulse);
 
     }
 
-    private void Update()
-    {
-       //nao usar input dentro do update
-       //input atraves de eventos
-
-    }
+    //private void OnCollisionEnter2D(Collision2D other)
+   // {
+    //   if(other.gameObject.CompareTag("Death"))
+    //    {
+    //        Death();
+     //   }
+  //  }
 }
