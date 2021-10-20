447b77888df3082c06759f2236fa92abe63d93d6
Ana Beatryz Yoshie <anabeatryz_y@outlook.com>
2021-10-20 12:41:33 -0300

    Pulo da Bolha

--- Assets/Scripts/BolhaFlutua.cs
+++ Assets/Scripts/BolhaFlutua.cs
@@ -1,40 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngineInternal;
+
 
 public class BolhaFlutua : MonoBehaviour
 {
     private Rigidbody2D _myRigidbody;
+    [SerializeField] private Vector2 bolhaForceInicio;
     [SerializeField] private Vector2 bolhaForce;
+    [SerializeField] private Vector2 bolhaForceInvert;
+    public bool Flutuar;
+    public bool B = true;
 
     void Start()
     {
         _myRigidbody = GetComponent<Rigidbody2D>();
+        _myRigidbody.IsSleeping();
+    }
 
+    public void Inicio()
+    {
+        if (B)
+        {
+            _myRigidbody.WakeUp();
+            B = false;
+            _myRigidbody.velocity = Vector2.zero;
+            _myRigidbody.AddForce(bolhaForceInicio, ForceMode2D.Impulse);
+        }
+        
     }
 
     public void Flutua()
     {
-        _myRigidbody.velocity = Vector2.zero;
-        _myRigidbody.AddForce(bolhaForce, ForceMode2D.Impulse);
-
+        if (Flutuar)
+        {
+            _myRigidbody.velocity = Vector2.zero;
+            _myRigidbody.AddForce(bolhaForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            _myRigidbody.velocity = Vector2.zero;
+            _myRigidbody.AddForce(bolhaForceInvert, ForceMode2D.Impulse);
+        }
     }
-    public void Inverte()
-    {
-        void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Paredes")
-            {
-                bolhaForce.x *= 1;
-            }
-    }
-}
-
-    
+       
     public void OnCollisionEnter2D(Collision2D collision)
     {
-     
+        if (collision.gameObject.tag == "ParedeE")
+        {
+            Flutuar = true;
+        }
+        if (collision.gameObject.tag == "ParedeD")
+        {
+            Flutuar = false;
+        }
         if (collision.gameObject.tag == "CollidFixo")
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
